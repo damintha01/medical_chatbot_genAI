d5fe08b1dc140810480c9b05c4839ae8dbbda478
Damintha Deshan Wijekoon <160304576+damintha01@users.noreply.github.com>
2026-02-07 14:36:10 +0530

    Add initial project files and update requirements
    - Created app.py, setup.py, and initial structure for src with __init__.py, helper.py, and prompts.py.
    - Added trials.ipynb to research directory.
    - Updated requirements.txt to include editable install.
    - Modified template.py to improve file handling and logging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot_genAI',
+    version='0.1.0',
+    author="damintha",
+    author_email="daminthadeshan01@gmail.com",
+    description='A medical chatbot powered by GenAI',
+    packages=find_packages(),
+    install_requires=[]
+)
--- template.py
+++ template.py
@@ -16,14 +16,16 @@ list_of_files = [
     "research/trials.ipynb"]
 
 for file in list_of_files:
-    file_path = os.path(file_path)
+    file_path = Path(file)
     filedir,filename = os.path.split(file_path)
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Created directory: {filedir} for file: {filename}")
 
-    if not os.path.exists(file_path):
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
         with open(file_path, 'w') as f:
             pass
         logging.info(f"Created file: {file_path}")
+    else:
+        logging.info(f"File already exists: {file_path}")
